21526d4f52e1070d67f78d9f76aa22a3b7b9a771
ecrtmn <neofobos@gmail.com>
2021-03-01 22:38:22 +0200

    static

--- library/settings.py
+++ library/settings.py
@@ -137,5 +137,3 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/root')
-
-django_heroku.settings(locals(), staticfiles=True)
